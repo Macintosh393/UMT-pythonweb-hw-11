92b3770cfa5532afdd817962e7d9799bb8fc0c83
Max Kosobutskyi <maxkosobutskyi@gmail.com>
2026-06-08 01:44:29 +0300

    Add auth and user services

--- src/conf/config.py
+++ src/conf/config.py
@@ -9,6 +9,10 @@ BASE_DIR = Path(__file__).resolve().parents[2]
 class Config(BaseSettings):
     DB_URL: str = ""
 
+    JWT_SECRET: str = "your_secret_key"
+    JWT_ALGORITHM: str = "HS256"
+    JWT_EXPIRATION_SECONDS: int = 3600
+
     model_config = SettingsConfigDict(
         extra="ignore",
         env_file=BASE_DIR / ".env",
